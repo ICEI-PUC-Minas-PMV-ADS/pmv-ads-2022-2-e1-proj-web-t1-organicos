c297ebbb5e55e150ffb05c69f72f034a96c3b4f5
Leticia Pauli <110863711+Leticiapauli@users.noreply.github.com>
2022-12-11 12:24:22 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-import { valida } from './validacao.js'
-
-const inputs = document.querySelectorAll('input')
-
-inputs.forEach(input => {
-    input.addEventListener('blur', (evento) => {
-        valida(evento.target)
-    })
-})
